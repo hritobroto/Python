64507c7005f5180900f0faa806950dc243bb57b2
Hritobroto Sanyal <60534062+hritobroto@users.noreply.github.com>
2021-07-01 14:09:54 +0530

    src

---
+++ YouTube/src/video_playlist.py
@@ -0,0 +1,10 @@
+"""A video playlist class."""
+
+
+class Playlist:
+    """A class used to represent a Playlist."""
+    pLists=[]
+    pListNs=[]
+    def __init__(self):
+        pLists=[]
+        pListNs=[]
